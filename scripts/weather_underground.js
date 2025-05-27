617ee811c2309df3a37c1b336a5cd512b744bb82
Chase Florell <chase.florell@gmail.com>
2025-05-26 22:24:10 -0700

    [CF] checkpoint

--- scripts/weather_underground.js
+++ scripts/weather_underground.js
@@ -1,14 +1,13 @@
 async function getWeather(location) {
     const apiKey = "6dfb9fed05d24b71bb9fed05d20b715d";
     // const apiUrl = `https://api.wunderground.com/api/${apiKey}/conditions/q/${location}.json`;
-    const apiUrl = `https://api.weather.com/v2/pws/observations/current?stationId=${location}&format=json&units=e&apiKey=${apiKey}`;
+    const apiUrl = `https://api.weather.com/v2/pws/observations/current?stationId=${location}&format=json&units=m&apiKey=${apiKey}`;
     try {
         const response = await fetch(apiUrl);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        const data = await response.json();
-        return data;
+        return await response.json();
     } catch (error) {
         console.error("Error fetching weather data:", error);
         return null;
