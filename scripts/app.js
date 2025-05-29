7c2480da10970aef80188940e81f1c8c3089358a
Chase Florell <chase.florell@gmail.com>
2025-05-29 13:17:37 -0700

    [CF] checkpoint

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,8 @@ var app = app || {};
 document.addEventListener('DOMContentLoaded', async function () {
 // Initial data load
     await app.weather.loadWeatherData();
+
+    // Set up automatic refresh every 5 minutes
+    setInterval(loadAndDisplayWeatherOverlay, 5 * 60 * 1000);
 });
 
